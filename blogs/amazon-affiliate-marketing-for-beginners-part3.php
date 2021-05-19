828b08dbbb47c162fa6c1351b1e16de6ef148ebe
DhirajBeriCoder <63449848+DhirajBeriCoder@users.noreply.github.com>
2021-05-19 14:58:03 +0530

    Add files via upload

---
+++ blogs/amazon-affiliate-marketing-for-beginners-part3.php
@@ -0,0 +1,121 @@
+<?php $title="Amazon affiliate marketing for beginners in 2020 (Part-3)"; $url="/blogs/amazon-affiliate-marketing-for-beginners-part3.php"; $description="Amazon affiliate marketing for beginners in 2020 – Make 1000$ per month (Part-3). Complete guide for beginners."; include '../menu.php';?>
+
+<div class="container my-4">
+<h1>
+    Amazon affiliate marketing for beginners in 2020 (Part-3)
+</h1>
+<p>
+    The best strategy is to first start a website, scale it, and then move to
+    the various verticals of YouTube and Instagram. This has given me the best
+    results. So, what is the process of making money from affiliate marketing
+    through a website.
+</p>
+<p>
+    Everything is explained in detail in our affiliate marketing course
+    playlist. But right now, I’ll be giving you a general overview of how all
+    these works. The process will be something like this. The first step is to
+    do niche research. I will help you find a low competition and profitable
+    money-making niche.
+</p>
+<p>
+    Then in the next lesson I’ll share with you how to start a pro affiliate
+    website. I’ll be sharing with you my tips, tricks and strategies that I’ve
+    learned in the last 7 years of doing affiliate marketing. Also, I’ll be
+    telling you the mandatory things that you need to follow so that your
+    account doesn’t get banned.
+</p>
+<p>
+    The third step is keyword research. I’ll share with you the exact strategy
+    that I use to find low competition keywords that make you money not in
+    years, but in months.
+</p>
+<p>
+    The fourth step is to write articles. Either you can write it yourself, but
+    I’ll also tell you how you can outsource it to someone else. I have spent
+    thousands of dollars in knowing the best article structure. How many words
+    your article should have? What should be the button color? I have tested
+    everything.
+</p>
+<p>
+    And I’ll be sharing the perfect article template with you in one of the
+    lessons. The next step will be signing up to the Amazon Associate account.
+    How to sign up? How to get approved? And how to not get banned. The next
+    lesson will cover the most important detail.
+</p>
+<p>
+    How to get your article on rank number one on Google search results. So,
+    let’s say your website is about guitar and you write an article like “best
+    guitars in India”. So, I’ll share with you all the strategies that I use to
+    rank number one for my target keyword.
+</p>
+<p>
+    And the last step is for you to send me a thank you email for introducing
+    you to affiliate marketing. I would love to see some success stories if I
+    like your site, I might even collaborate with you for other projects. So
+    now let’s talk about money. How much many can you make?
+</p>
+<p>
+    Now this depends entirely on you. How much time you are willing to invest
+    on your website. The more time you invest, the more money you will be
+    making. If you follow our complete playlist then I can tell you for sure
+    that you will be making money in few months.
+</p>
+<p>
+    In the beginning, it might look a bit complicated because you are new at
+    this. But given a few days of implementing, you will become a pro. Once you
+    scale the website, then you already know all the secrets. Then you can
+    create many more websites, build a team, and scale even further.
+</p>
+<p>
+    Like I started as a one-man-army, but today I have 9 full time people
+    working on my affiliate websites. You are planting a tree. Once it’s grown
+    up, all you have to do is spend a few hours on your website and it will
+    keep giving you fruits for many years to come.
+</p>
+<p>
+    Now let’s talk about common affiliate marketing mistakes. New affiliate
+    marketers make these following common mistakes. Even I made them when I got
+    started. The first mistake is not showing patience. You won’t start making
+    money on day one. Rather, slowly and steadily you will be scaling.
+</p>
+<p>
+    The second mistake is not implementing. Majority of you will just be
+    watching the lessons, but very few of you will be actually implementing.
+    But the people who implement, will be getting the best results. So, make
+    sure you don’t just watch the lessons, but also implement everything I tell
+    you.
+</p>
+<p>
+    And not just implementing, you’ll be getting a lot of questions in your
+    mind. So, don’t just implement, you can ask your questions in the Facebook
+    group or you can also ask them in our YouTube live sessions. And I’ll try
+    my best to answer all of you guys.
+</p>
+<p>
+    Third mistake is getting greedy. Now few of you might watch our complete
+    free affiliate marketing course playlist and then start implementing
+    everything on multiple websites. If you’re a beginner or just starting out,
+    even if you are an experienced affiliate, I would recommend you to open
+    only one single website, and try to focus entirely on that.
+</p>
+<p>
+    Your goal should first be able to create a website and scale it. Learn all
+    the advance strategies, then build a team, and then open multiple websites.
+    You don’t have to work hard, you have to work smartly. I have already made
+    the mistakes, so you don’t have to make the same mistakes again.
+</p>
+<p>
+    Rather follow the working strategies that actually work. One big advice
+    that I can give to all of you is that practical is what matters the most.
+    Don’t just watch the videos but also implement everything that I tell you
+    in this free affiliate marketing mastery course.
+</p>
+<p>
+    This is the right thing for you guys. Don’t be confused, I am here for you.
+    I’m so happy to have a new member who will be our future success story.
+    I’ll make sure that this journey of helping you, making money from
+    affiliate marketing is fun, exciting and as easy as possible.
+</p>
+</div>
+
+<?php include '../footer.php';?>
---
+++ blogs/roadmap-android-developer.php
@@ -0,0 +1,13 @@
+<?php $title="How to become an Android developer | Complete Roadmap"; $url="/blogs/roadmap-android-developer.php"; $description="Complete Roadmap for Android Development for Beginners - Today we'll see how to become an android developer in hindi. This Android development roadmap in hindi will be a step by step guide for beginners to learn Android Development."; include '../menu.php';?>
+<div class="container my-4">
+<h1>
+	How to become an Android developer | Complete Roadmap
+</h1>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/A14BLDzNy2k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+<p>Complete Roadmap for Android Development for Beginners - Today we'll see how to become an android developer in hindi. This Android development roadmap in hindi will be a step by step guide for beginners to learn Android Development.</p>
+
+
+</div>
+<?php include '../footer.php';?>
